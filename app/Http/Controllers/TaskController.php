140c4c04facaf1677878ddcc1b6f452a3aedd736
introqt <introtheintro@gmail.com>
2017-10-10 17:44:45 +0300

    TaskController создание

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Task;
 use App\Http\Requests;
 
 class TaskController extends Controller
 {
-    //
+    
+    public function __construct() {
+        $this->middleware('auth');// Проверять авторизацию при доступе к данному контроллеру
+    }
+    
+    public function index() {
+        $task=Task::all();
+        return view('task', ['task'=>$tasks]);
+    }
+    
+    public function store(Requests $request){
+        
+    }
+    
+    public function destroy (Task $task){
+        
+    }
 }
