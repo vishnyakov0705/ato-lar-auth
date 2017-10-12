26f1fcd82baa33ae681365fec5998d71c2db9f1b
introqt <introtheintro@gmail.com>
2017-10-12 17:51:56 +0300

    продолжили знакомство с ларой

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,24 +5,68 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Task;
 use App\Http\Requests;
-
+use App\Repositories\TaskRepository;
 class TaskController extends Controller
 {
-    
-    public function __construct() {
+    /**
+   * Экземпляр TaskRepository.
+   *
+   * @var TaskRepository
+   */
+  protected $tasks;
+  
+    public function __construct(TaskRepository $tasks) 
+    {
         $this->middleware('auth');// Проверять авторизацию при доступе к данному контроллеру
+        $this->tasks=$tasks;
     }
     
-    public function index() {
-        $task=Task::all();
-        return view('task', ['task'=>$tasks]);
+    /**
+    * Показать список всех задач пользователя.
+    *
+    * @param  Request  $request
+    * @return Response
+    */
+    public function index(Request $request)
+    {
+      //$tasks = $request->user()->tasks()->get();
+      return view('tasks.index', [
+        'tasks' => $this->tasks->forUser($request->user()),
+      ]);
     }
     
-    public function store(Requests $request){
+     /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+      $this->validate($request, [
+        'name' => 'required|max:255',
+      ]);
+
+        $request->user()->tasks()->create([
+        'name' => $request->name,
+        ]);
+
+  return redirect('/tasks');
+    }
+    
+     /**
+     * Уничтожить заданную задачу.
+     *
+     * @param  Request  $request
+    * @param  Task  $task
+    * @return Response
+    */
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
         
-    }
-    
-    public function destroy (Task $task){
-        
+        // Удаление задачи...
+        $task->delete();
+        return redirect('/tasks');
     }
 }
---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\User;
+use App\Task;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    /**
+     * Определяем, может ли данный пользователь удалить данную задачу.
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+}
+
